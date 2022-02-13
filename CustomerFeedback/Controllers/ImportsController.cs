eb3e4c0fd21e848e18525c7e80c3e4f2570260d4
bunda3d <bunda3@yahoo.com>
2022-02-13 08:48:53 -0600

    Update to have Import class, controller, and upload view

--- CustomerFeedback/Controllers/ImportsController.cs
+++ CustomerFeedback/Controllers/ImportsController.cs
@@ -3,15 +3,26 @@ using CsvHelper;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using CustomerFeedback.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace CoreWithCSVHelper.Controllers
+namespace CustomerFeedback.Controllers
 {
-  public class StudentsController : Controller
+  public class ImportsController : Controller
   {
-    [HttpGet]
-    public IActionResult Index(List<Student> students = null)
+    private readonly CSATContext _context;
+
+    public ImportsController(CSATContext context)
     {
-      students = students == null ? new List<Student>() : students;
+      _context = context;
+    }
+
+    public List<Import> Imports { get; set; }
+
+    [HttpGet]
+    public IActionResult Index(List<Import> imports = null)
+    {
+      imports = imports == null ? new List<Import>() : imports;
       return View();
     }
 
@@ -20,7 +31,7 @@ namespace CoreWithCSVHelper.Controllers
     {
       #region Upload CSV
 
-      if (file == null) return View("Choose a file before submitting");
+      if (file == null) return NotFound("No file submitted. Make sure you are selecting a file for upload and try again.");
 
       string fileName = $"{ hostingEnvironment.WebRootPath }\\csvimport\\{file.FileName}";
       using (FileStream fileStream = System.IO.File.Create(fileName))
@@ -31,13 +42,13 @@ namespace CoreWithCSVHelper.Controllers
 
       #endregion Upload CSV
 
-      var students = this.GetStudentList(file.FileName);
-      return View("Index", students);
+      var imports = this.GetImportList(file.FileName);
+      return View("Index", imports);
     }
 
-    private List<Student> GetStudentList(string fileName)
+    private List<Import> GetImportList(string fileName)
     {
-      List<Student> students = new List<Student>();
+      List<Import> imports = new List<Import>();
 
       #region Read CSV
 
@@ -49,9 +60,12 @@ namespace CoreWithCSVHelper.Controllers
         csv.ReadHeader();
         while (csv.Read())
         {
-          var student = csv.GetRecord<Student>();
-          students.Add(student);
+          var import = csv.GetRecord<Import>();
+          imports.Add(import);
         }
+        Imports = imports;
+
+        return imports;
       }
 
       #endregion Read CSV
@@ -62,12 +76,26 @@ namespace CoreWithCSVHelper.Controllers
       using (var write = new StreamWriter(path + "\\NewFile.csv"))
       using (var csv = new CsvWriter(write, CultureInfo.InvariantCulture))
       {
-        csv.WriteRecords(students);
+        csv.WriteRecords(imports);
       }
 
       #endregion Create CSV
 
-      return students;
+      return imports;
     }
+
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public async Task<IActionResult> Create(Imports)
+    //{
+    //  var imports = GetImportList();
+    //  foreach (Import import in imports)
+    //  {
+    //    _context.Add(import);
+    //    await _context.SaveChangesAsync();
+    //    return RedirectToAction(nameof(Index));
+    //  }
+    //  return View(import);
+    //}
   }
 }
---
+++ CustomerFeedback/Models/Import.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CustomerFeedback.Models
+{
+  public class Import
+  {
+    [Ignore]
+    public int Id { get; set; }
+
+    [Index(0)]
+    [Name("Respondent ID")]
+    public long? RespondentID { get; set; }
+
+    [Index(1)]
+    public int? CollectorID { get; set; }
+
+    [Name("Email Address")]
+    public string? EmailAddress { get; set; } = "";
+
+    [Index(9)]
+    [Name("Title")]
+    public string? Title { get; set; } = "";
+
+    [Index(10)]
+    public string? Location { get; set; } = "";
+
+    [Index(12)]
+    [Name("A1")]
+    public string? QuestionResponse1 { get; set; } = "";
+
+    [Index(13)]
+    [Name("A2")]
+    public string? QuestionResponse2 { get; set; } = "";
+
+    [Index(14)]
+    [Name("A3")]
+    public string? QuestionResponse3 { get; set; } = "";
+  }
+}
